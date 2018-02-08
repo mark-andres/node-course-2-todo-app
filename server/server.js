b5f53559242ce7e571e01c19d16c2ca8aec9ca6c
Mark Andres <mandres12@gmail.com>
2018-02-07 18:16:00 -0600

    Added GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -27,6 +28,23 @@ app.get('/todos', (req, res) => {
     }, (e) => {
         res.statusMessage(400).send(e);
     });
+});
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if (todo) {
+            res.status(200).send({todo});
+        } else {
+            res.status(404).send();
+        }
+    }).catch(() => {
+        res.status(400).send();
+    })
 });
 
 app.listen(3000, () => {
