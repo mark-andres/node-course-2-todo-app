d3e02095f0890d891c31f466c1ab89ea93dfc0a7
Mark Andres <mandres12@gmail.com>
2018-02-14 12:05:22 -0600

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -3,12 +3,12 @@ require('./config/config');
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
-const {ObjectID} = require('mongodb');
+const { ObjectID } = require('mongodb');
 
-var {mongoose} = require('./db/mongoose');
-var {Todo} = require('./models/todo');
-var {User} = require('./models/user');
-var {authenticate} = require('./middleware/authenticate');
+var { mongoose } = require('./db/mongoose');
+var { Todo } = require('./models/todo');
+var { User } = require('./models/user');
+var { authenticate } = require('./middleware/authenticate');
 
 var app = express();
 const port = process.env.PORT || 3000;
@@ -29,7 +29,7 @@ app.post('/todos', (req, res) => {
 
 app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
-        res.send({todos});
+        res.send({ todos });
     }, (e) => {
         res.statusMessage(400).send(e);
     });
@@ -43,7 +43,7 @@ app.get('/todos/:id', (req, res) => {
 
     Todo.findById(id).then((todo) => {
         if (todo) {
-            res.status(200).send({todo});
+            res.status(200).send({ todo });
         } else {
             res.status(404).send();
         }
@@ -60,7 +60,7 @@ app.delete('/todos/:id', (req, res) => {
 
     Todo.findByIdAndRemove(id).then((todo) => {
         if (todo) {
-            res.status(200).send({todo});
+            res.status(200).send({ todo });
         } else {
             res.status(404).send();
         }
@@ -84,12 +84,12 @@ app.patch('/todos/:id', (req, res) => {
         body.completedAt = null;
     }
 
-    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    Todo.findByIdAndUpdate(id, { $set: body }, { new: true }).then((todo) => {
         if (!todo) {
             return res.status(404).send();
         }
 
-        res.send({todo});
+        res.send({ todo });
     }).catch((e) => {
         res.statusMessage(400).send();
     });
@@ -109,7 +109,20 @@ app.post('/users', (req, res) => {
 });
 
 app.get('/users/me', authenticate, (req, res) => {
-    res.send(req.user);    
+    res.send(req.user);
+});
+
+// POST /users/login (email, password)
+app.post('/users/login', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+
+    User.findByCredentials(body.email, body.password).then((user) => {
+        return user.generateAuthToken().then((token) => {
+            res.header('x-auth', token).send(user);
+        });
+    }).catch((e) => {
+        res.status(400).send();
+    });
 });
 
 app.listen(port, () => {
