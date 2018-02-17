55f8f2f4336e9eabeedc34e7877aac59c2fe101a
Mark Andres <mandres12@gmail.com>
2018-02-17 08:21:05 -0600

    Updated seed data and fixed tests (pass 1)

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -257,7 +257,7 @@ describe('POST /users/login', () => {
                 }
 
                 User.findById(users[1]._id).then((user) => {
-                    expect(user.tokens[0]).toInclude({
+                    expect(user.tokens[1]).toInclude({
                         access: 'auth',
                         token: res.headers['x-auth']
                     });
@@ -283,7 +283,7 @@ describe('POST /users/login', () => {
                 }
                 
                 User.findById(users[1]._id).then((user) => {
-                    expect(user.tokens.length).toBe(0);
+                    expect(user.tokens.length).toBe(1);
                     done();
                 }).catch((e) => done(e));
             });
